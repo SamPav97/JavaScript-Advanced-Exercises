f6cd1aab4d3a9c43b56e34927cffdce28a90b6b4
Samuil <samuil.pavlevchev@gmail.com>
2022-06-06 16:15:35 +0100

    Advanced Problems

---
+++ 03. Exercise Objects and Composition/10 heroes.js
@@ -0,0 +1,46 @@
+function heroes() {
+    return {
+        mage : function (name) {
+            return{
+                name,
+                health: 100,
+                mana: 100,
+                cast: function (spell) {
+                    this.mana -= 1
+                    console.log(`${this.name} cast ${spell}`)}
+            }
+        },
+        fighter: function (name) {
+           return {
+               name,
+               health: 100,
+               stamina: 100,
+               fight: function () {
+                   this.stamina -= 1
+                   console.log(`${this.name} slashes at the foe!`);
+               }
+
+           }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+let create = heroes();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball")
+scorcher.cast("thunder")
+scorcher.cast("light")
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight()
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
+
