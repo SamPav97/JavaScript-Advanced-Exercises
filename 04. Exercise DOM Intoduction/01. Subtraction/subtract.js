dce97efd625c3c24b975a4856915130e06a27f59
Samuil <samuil.pavlevchev@gmail.com>
2022-06-10 19:23:36 +0100

    Advanced Problems

---
+++ 04. Exercise DOM Intoduction/01. Subtraction/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+    const num1 = document.getElementById("firstNumber").value;
+    const num2 = document.getElementById("secondNumber").value;
+
+    let res = Number(num1) - Number(num2);
+
+    document.getElementById("result").innerHTML = res // can also do txt content
+}
