34f215b8dd43eaf6447098bdd63320a8553788bb
Samuil <samuil.pavlevchev@gmail.com>
2022-06-30 12:12:34 +0100

    Problems

---
+++ 08. Exercise Classes and Attributes/02 persons.js
@@ -0,0 +1,31 @@
+function getPersons() {
+
+    class Person {
+        constructor(firstName, lastName, age, email){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+    
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+    
+    let lst = [];
+    let anna = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    let softuni = new Person('SoftUni');
+    let stephan = new Person('Stephan', 'Johnson', 25);
+    let gabriel = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    lst.push(anna);
+    lst.push(softuni);
+    lst.push(stephan);
+    lst.push(gabriel);
+
+    return lst
+
+}
+
+console.log(getPersons());
